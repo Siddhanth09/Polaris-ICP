8b6df20a3b92c2a18d4e1ec15c5a7e0db0c15e4e
muskbuster <sudeepkamat79@gmail.com>
2023-11-15 08:39:39 +0000

    Update interval and add proof verification function

--- Sequencer/verifier.js
+++ Sequencer/verifier.js
@@ -1,6 +1,27 @@
-// TO write verifier for merkel tree root 
 const { MerkleTree } = require('merkletreejs');
 const SHA256 = require('crypto-js/sha256');
-const { JsonRpcProvider } = require('ethers');
 
-// take inputs 
+function verifyProof(proof, leaf, root) {
+  // Convert the leaf and root to buffers
+  const leafBuffer = Buffer.from(leaf, 'hex');
+  const rootBuffer = Buffer.from(root, 'hex');
+
+  // Create a Merkle tree with a single leaf
+  const tree = new MerkleTree([leafBuffer], SHA256);
+
+  // Convert the proof to an array of buffers
+  const proofBuffers = proof.map((item) => Buffer.from(item, 'hex'));
+
+  // Verify the proof against the root
+  const isValid = tree.verify(proofBuffers, leafBuffer, rootBuffer);
+
+  return isValid;
+}
+
+// Example usage:
+const proof = JSON.parse(stringProof); // Assuming stringProof is the proof in JSON format
+const leaf = leaves[0]; // Assuming leaves is an array of leaf hashes in hex format
+const root = '...'; // Replace with the actual Merkle root
+
+const isValid = verifyProof(proof, leaf, root);
+console.log('Proof Verification Result:', isValid);
